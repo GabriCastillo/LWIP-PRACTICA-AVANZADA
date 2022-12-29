19997dd84601edc28d6aa092c56683c678d1f4e4
Gabriel Castillo <castillogabriel005@gmail.com>
2022-12-29 18:42:41 +0100

    Update ej1.c

--- lwip-contrib/apps/practica/ej1.c
+++ lwip-contrib/apps/practica/ej1.c
@@ -305,7 +305,8 @@ void echo_send(struct tcp_pcb *tpcb, struct echo_state *es)
   {
     ptr = es->p;
 
-    char text[ptr->len] = ptr->payload;
+    char text[ptr->len] = "";
+    text = ptr->payload;
     printf(text);
     printf(ptr->len);
     bool i = false;
