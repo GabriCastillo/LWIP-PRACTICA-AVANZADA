ea9ef3fefcba78deaadca2dedaae8b24f33e3b55
Gabriel Castillo <castillogabriel005@gmail.com>
2022-12-30 16:06:05 +0100

    Update ej1.c

--- lwip-contrib/apps/practica/ej1.c
+++ lwip-contrib/apps/practica/ej1.c
@@ -307,9 +307,9 @@ void echo_send(struct tcp_pcb *tpcb, struct echo_state *es)
     bool ii = false;
     int k = 0;
 
-    while (pChar[k] != '\0')
+    while (pChar[k] != '\0' && !i)
     {
-
+      printf("%c", pChar[k]);
       if ("%c", pChar[k] == '0' || "%c", pChar[k] == '1')
       {
         printf("Paso");
